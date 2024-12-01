21d0c6f3e37bf120d4d72acf9249a56017b12fe5
agni-7 <190278002+agni-7@users.noreply.github.com>
2024-12-01 21:39:33 +0100

    missed part two lol

--- src/Day01.kt
+++ src/Day01.kt
@@ -10,14 +10,18 @@ fun main() {
         val (firstList, secondList) = parseLists(input)
         val sortedFirst = firstList.sorted()
         val sortedSecond = secondList.sorted()
-        val difference = sortedFirst.zip(sortedSecond).fold(0) {acc, (a, b) -> acc + abs(a - b) }
-        return difference
+        return sortedFirst.zip(sortedSecond).fold(0) {acc, (a, b) -> acc + abs(a - b) }
+    }
+
+    fun part2(input: List<String>): Int {
+        val (firstList, secondList) = parseLists(input)
+        return firstList.fold(0) { acc, a -> acc + (a * secondList.count { it == a })}
     }
 
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 11)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
+    part2(input).println()
 }
